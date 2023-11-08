77a0fa0a8df658cbba4d37f8cc097b8176012fb9
Michael Schreiber <michaelpschreiber@gmail.com>
2023-11-07 21:12:29 -0800

    big changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
 import { AppComponent } from './app.component';
-import { ShoppingListComponent } from './ShoppingComponents/shopping-list/shopping-list.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingListComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent
+    
   ],
   imports: [
     BrowserModule,
