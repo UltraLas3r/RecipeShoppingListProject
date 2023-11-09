132ceb108f3ebd9368fd493b6a4bb177eddc71ae
Michael Schreiber <michaelpschreiber@gmail.com>
2023-11-08 20:41:41 -0800

    added some more functionality and styling

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import {Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Hamburger', 'This is a Test', 'https://s7d1.scene7.com/is/image/mcdonalds/Header_Hamburger_832x472:1-3-product-tile-desktop?wid=763&hei=472&dpr=off')
+  ];
+
+  constructor(){ }
 
 }
